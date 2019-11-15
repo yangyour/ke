e9e8f0437a08d319cfd2bbf3004bebc15b4cb317
loveying <312054439@qq.com>
2019-11-15 16:51:28 +0800

    部署bug修改

--- dbblog-frontend/src/components/header/SimpleHeader/SimpleHeader.vue
+++ dbblog-frontend/src/components/header/SimpleHeader/SimpleHeader.vue
@@ -10,7 +10,7 @@
       <div id="header"  v-show="show">
     <router-link id="logo" to="/">
       <img src="../../../assets/logo.png">
-      <span class="title">DB's Blog</span>
+      <span class="title">WU's Blog</span>
       <span class="motto">success belongs to the persevering</span>
     </router-link>
     <ul id="nav">
